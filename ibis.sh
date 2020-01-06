5873ab6837ddcdafaf674d3cae1f2206124d24ee
N1ght420 <n1ghtpe0ple@protonmail.com>
2020-01-06 07:18:18 +0700

    Update ibis.sh

--- ibis.sh
+++ ibis.sh
@@ -8,8 +8,11 @@ lightgreen='\e[1;32m'
 white='\e[1;37m'
 red='\e[1;31m'
 yellow='\e[1;33m'
+mkdir ~/.themes
+mkdir ~/.icons
 
 banner(){
+	clear
 	echo -e ""
 	echo -e "$white    ______    _    $red ______            __    "
 	echo -e "$white   /  _/ /_  (_)___$red/_  __/___  ____  / /____"
